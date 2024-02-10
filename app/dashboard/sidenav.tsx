65ef228fd3f65e6ce4486d3b949c490f7851b4d1
PVG <vitalikpirlog@yahoo.com>
2024-02-11 00:11:06 +0200

    commit

--- app/dashboard/sidenav.tsx
+++ app/dashboard/sidenav.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import NavLinks from '@/app/ui/dashboard/nav-links';
+import NavLinks from '@/app/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 
@@ -8,7 +8,7 @@ export default function SideNav() {
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
-        href="/"
+        href="/public"
       >
         <div className="w-32 text-white md:w-40">
           <AcmeLogo />
